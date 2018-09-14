11f9f508d4a1d24a0167ada28b63212a0e627bf7
Mary Jack <maryjackinthebox@gmail.com>
2018-09-14 19:07:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
      if @@items.include?(item)
        @@cart << item
        resp.write "added #{item}\n"
-     else resp.write "Item not available"
+     else resp.write "We don't have that item"
      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
